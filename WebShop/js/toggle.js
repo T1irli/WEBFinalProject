1fa9e1847dfec05b206391278b260bebcd81c299
T1irli <hnidyidima@gmail.com>
2023-04-17 23:15:56 +0300

    toggle done

--- WebShop/js/toggle.js
+++ WebShop/js/toggle.js
@@ -2,8 +2,30 @@ let toggle= document.getElementsByClassName('navbar-toggler')[0]
 let sidebar = document.getElementsByClassName('sidebar')[0]
 
 toggle.addEventListener('click', () => {
-    toggle.ariaExpanded = toggle.ariaExpanded == "true"? "false" : "true"
-    if(toggle.ariaExpanded === "true"){
-        
-    }
+    toggle.className = toggle.className == "navbar-toggler open"? 
+    "navbar-toggler close" : "navbar-toggler open"
+    let sidebar = document.getElementsByClassName('sidebar')[0]
+    checkToggle()
 })
+
+onresize = (event) => {
+    let sidebar = document.getElementsByClassName('sidebar')[0]
+    if(innerWidth > 768){
+        sidebar.style.display = 'block'
+        sidebar.style.width = '300px'
+    }
+    else{
+        checkToggle()
+    }
+};
+
+function checkToggle(){
+    if(toggle.className === "navbar-toggler open"){
+        sidebar.style.display = 'block'
+        sidebar.style.width = '100vh'
+    }
+    else{
+        sidebar.style.display = 'none'
+        sidebar.style.width = 0
+    }
+}
